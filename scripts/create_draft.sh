b41aae5ae6d05d8acc65a20847bd9c6f97d00750
Borja Lorente <blorente.me@gmail.com>
2020-07-26 17:57:02 +0200

    Add create_draft.sh

--- scripts/create_draft.sh
+++ scripts/create_draft.sh
@@ -4,9 +4,10 @@ source scripts/funcs.sh
 date=$(date_now)
 
 title=$1
+template_path=$2
 title_sanitized=$(echo "${title}" | tr " " "-" | tr "[:upper:]" "[:lower:]")
 
-cat ./scripts/1_percent_template | \
+cat "${template_path}" | \
   sed "s:__TITLE__:${title}:g" | \
   sed "s:__TITLE_URL__:${title_sanitized}.html:g" | \
   sed "s:__DATE__:${date}:g" > "_drafts/${date}-${title_sanitized}.md"
