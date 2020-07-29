f9b22c2fd6790357d91a1b6c85afa54294434782
Borja Lorente <blorente.me@gmail.com>
2020-07-29 10:30:05 +0200

    Fix create_draft

--- scripts/create_draft.sh
+++ scripts/create_draft.sh
@@ -8,6 +8,6 @@ template_path=$2
 title_sanitized=$(echo "${title}" | tr " " "-" | tr "[:upper:]" "[:lower:]")
 
 cat "${template_path}" | \
-  sed "s:__TITLE__:${title}:g" | \
-  sed "s:__TITLE_URL__:${title_sanitized}.html:g" | \
-  sed "s:__DATE__:${date}:g" > "_drafts/${date}-${title_sanitized}.md"
+  sed "s/__TITLE__/${title}/g" | \
+  sed "s/__TITLE_URL__/${title_sanitized}.html/g" | \
+  sed "s/__DATE__/${date}/g" > "_drafts/${date}-${title_sanitized}.md"
